03114a97b8b6be2d8984d524a9c7f55e97118359
lynnga <lingamliel@gmail.com>
2022-09-01 22:29:39 -0500

    finished project

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var characterlength = 0;
 var choiceArray = [];
 
-// var specialCharArr = ["/ !,",#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,`,{,|,},~,","];
-// var lowCassArr = [a','b','c','d','e','f','g','h','i,'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z"];
-// var upperCaseArr = [" A, B, C, D, E, F, 'G', 'H', 'I,'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z];
-var numberArr  = [1,2,3,4,5,6,7,8,9,0];
+// var SpecialCharArr =['!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']', '^','`','{','|','}','~','"'];
+// // '\^_`{|}~"
+var specialCharArr = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '`', '{', '|', '}', '~', '"'];
+var lowCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 // // Assignment code here
 
@@ -47,25 +49,32 @@ function getPrompts() {
     alert("character length has to be a number, 8-128 digits. Please try again")
     return false;
   }
-  var lowercaseChoice = confirm("would you like lowercase letters in your password?")
-  if (lowercaseChoice ===true) {
-    choiceArr = choiceArr.concat(lowerCaseArr);
+
+  var lowerCaseChoice = confirm("would you like lowercase letters in your password?")
+  if (lowerCaseChoice === true) {
+    choiceArr = choiceArr.concat(lowCaseArr);
+  }
+  var upperChoice = confirm("Would you like uppercase letters in your password?")
+  if (upperChoice === true) {
+    // if (confirm("would you like uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCaseArr);
+  }
+  var SpecialCharChoice = confirm("would you like to use special characters in your password?")
+  if (SpecialCharChoice === true) {
+    // if (confirm("would you like special charcters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharArr);
   }
 
-  if (confirm("would you like uppercase letters in your password?")) {
-    choiceArr = choiceArr.concat(UpperCaseArr);
-  }
-
-  if (confirm("would you like special charcters in your password?")) {
-    choiceArr = choiceArr.concat(SpecialCharArr);
-  }
-
-  if (confirm("would you like numbers in your password?")) {
+  var numberChoice = confirm("would you like to use numbers in your password?")
+  if (numberChoice === true) {
+    // if (confirm("would you like numbers in your password?")) {
     choiceArr = choiceArr.concat(numberArr);
   }
-
-if(lowercaseChoice ===false && upperChoice ===false && SpecialCharChoice ===false && numberChoice=== false) {
-  alert("Must choose one type of caracter to have in a password") 
-  getPrompts()
-}
+ 
+  
+  if (lowerCaseChoice === false && upperChoice === false && SpecialCharChoice === false && numberChoice === false) {
+    alert("Must choose one type of charcter to have in a password")
+    getPrompts()
+  }
+  return true
 }
